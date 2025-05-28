5905a6c5230e9c913281d46dddb58879279224e5
Ahmed Farouk <ahmedmfarouk252@gmail.com>
2025-05-28 13:33:21 +0000

    Update 2 files
    - /frontend/src/setupTests.js
    - /.gitlab-ci.yml

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,11 +1,9 @@
-// Polyfill TextEncoder/TextDecoder (for MSW in Jest)
 import { TextEncoder, TextDecoder } from 'util';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-// MSW Server Setup
-import { server } from './mocks/server';
 
+import { server } from './mocks/server';
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+afterAll(() => server.close();
