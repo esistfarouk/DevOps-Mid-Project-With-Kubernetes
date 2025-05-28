5f00b97f6d53e4ee39f6e8490c404c5745605789
Ahmed Farouk <ahmedmfarouk252@gmail.com>
2025-05-28 15:58:30 +0000

    Update file setupTests.js

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -4,3 +4,9 @@ import { server } from './mocks/server';
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+// Polyfill for TextEncoder and TextDecoder
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
